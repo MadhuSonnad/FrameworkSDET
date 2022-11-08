f447ef77847cc9cd52f500811f45e12daa24315d
MadhuSonnad <115971662+MadhuSonnad@users.noreply.github.com>
2022-11-08 09:07:55 +0530

    Update testNg_priority.java

--- SELENIUM WORKS/WORKSHOP/TestNG/testNg_priority.java
+++ SELENIUM WORKS/WORKSHOP/TestNG/testNg_priority.java
@@ -3,26 +3,22 @@ package TestNG;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class testNg_priority {
-
-
+public class testNg_priority 
+{
 	@Test(priority=3)
 	public void test1()
 	{
-		Reporter.log("hello",true);
-		
+		Reporter.log("hello",true);	
 	}
 	@Test(priority=1)
 	public void test2()
 	{
 		Reporter.log("bye",true);
-		
-        Test(priority=2)
+	}
+        @Test(priority=2)
 	public void test3()
 	{
-		Reporter.log("bye",hey);
-		
-}
-		
+		Reporter.log("bye",false);		
+	}	
 }
 }
