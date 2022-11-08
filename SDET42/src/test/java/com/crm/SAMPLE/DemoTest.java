6fccb4987222c81da805696b2eee427f5c061087
MadhuSonnad <madhu.sonnad@gmail.com>
2022-11-08 08:34:04 +0530

    Sucess

---
+++ SDET42/src/test/java/com/crm/SAMPLE/DemoTest.java
@@ -0,0 +1,14 @@
+package com.crm.SAMPLE;
+
+import org.testng.annotations.Test;
+
+public class DemoTest
+
+{
+	@Test(groups="smokeTest")
+public void method()
+{
+	System.out.println("verson cheking");
+}
+		
+}
---
+++ SDET42/src/test/java/com/crm/SAMPLE/TesterTest.java
@@ -0,0 +1,13 @@
+package com.crm.SAMPLE;
+
+import org.testng.annotations.Test;
+
+public class TesterTest
+{
+	@Test(groups={"smokeTest","regressionTest"}) 
+	public void methods()
+	{
+		System.out.println("verson waiting");
+	}
+	}
+
